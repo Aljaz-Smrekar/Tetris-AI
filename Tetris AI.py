42f810b64a98938509dabf325c6d23203b7cbd4d
Aljaz Smrekar <aljazsmrekar11@gmail.com>
2024-04-11 11:52:24 +1200

    Back to normal

--- Tetris AI.py
+++ Tetris AI.py
@@ -1,5 +1,1 @@
 def tetris_ai():
-
-
-
-"https://gymnasium.farama.org/environments/atari/tetris/?fbclid=IwAR3lzPuN89eXMrG8r29Fgsi4xdGO91vm7m6FxX4l98oCImTWEvGidZQfcAo_aem_AUPeWinBqy7IzJX-4ROj8UiyH9YwT_ylddDcAJTZIicYtbM35Xg6gf8Y8uQ8wnh6aed0-7WuBt7PwXpg7fVdBLuY"
