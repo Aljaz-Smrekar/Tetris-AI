27f4700232d8b224f7ade8bdbae137bf6f3b7583
Aljaz Smrekar <aljazsmrekar11@gmail.com>
2024-04-11 12:10:58 +1200

    set up the start of the AI

--- Tetris AI.py
+++ Tetris AI.py
@@ -1,1 +1,19 @@
+
+import gymnasium as gym
+env = gym.make("ALE/Tetris-v5", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
+
 def tetris_ai():
+    """The start of our Tetris AI that will beat the game"""
+
+
+
