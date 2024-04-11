7256473c52f60338d729403442088f55bc44952b
Aljaz Smrekar <aljazsmrekar11@gmail.com>
2024-04-11 15:49:08 +1200

    AI for stick holding to be implemented for tetris

--- Tetris AI.py
+++ Tetris AI.py
@@ -1,4 +1,5 @@
 
+import tensorflow as tf
 import gymnasium as gym
 env = gym.make("ALE/Tetris-v5", render_mode="human")
 observation, info = env.reset()
@@ -12,8 +13,21 @@ for _ in range(1000):
 
 env.close()
 
-def tetris_ai():
+def __init__(self):
+    """Things inside of Tetris"""
+    self.game_over = False
+    self.reward = 0
+    self.line_cleared = 300
+    self.tetris = 
+def tetris_ai(self):
     """The start of our Tetris AI that will beat the game"""
 
+def scoring(self):
+    
+
+
+def end(self):
+    if self.game_over == True:
+        return 
 
 
